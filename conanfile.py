9c441b50b910b3e388e98157b3aba6eb1d7e09fb
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-01-03 20:04:56 +0300

    Updated CMakeLists.txt files and conanfile.py file for Conan 2.0 compatibility.

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,26 @@
-from conans import ConanFile, CMake, tools
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.files import copy
 
 
 class MorphologicalDisambiguationConan(ConanFile):
-    name = "MorphologicalDisambiguation"
+    name = "morphological_disambiguation"
     version = "1.0.0"
-    requires = ["AnnotatedTree/1.0.0", "NGram/1.0.0"]
+    requires = ["annotated_tree/1.0.0",
+                "n_gram/1.0.0",
+                "morphological_analysis/1.0.0",
+                "dictionary/1.0.0",
+                "corpus/1.0.0",
+                "annotated_sentence/1.0.0",
+                "named_entity_recognition/1.0.0",
+                "prop_bank/1.0.0",
+                "frame_net/1.0.0",
+                "dependency_parser/1.0.0",
+                "senti_net/1.0.0",
+                "xml_parser/1.0.0",
+                "word_net/1.0.0"]
     license = "GPL License"
     author = "Olcay Taner Yildiz olcay.yildiz@ozyegin.edu.tr"
     url = "https://github.com/StarlangSoftware/TurkishMorphologicalDisambiguation-CPP"
@@ -13,25 +29,28 @@ class MorphologicalDisambiguationConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
     exports_sources = "src/*", "Test/*"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", keep_path=False)
-        self.copy("*MorphologicalDisambiguation.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        copy("*.h", keep_path=True, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
+        copy("*.a", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.a")
+        copy("*.so", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.so")
+        copy("*.dylib", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.dylib")
+        copy("*.dll", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "bin"), pattern="*.dll")
 
     def package_info(self):
         self.cpp_info.libs = ["MorphologicalDisambiguation"]
